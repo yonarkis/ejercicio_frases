28fd3de1e45b17a6ae9e6c20f463cd96034c8508
yonarkis <yonarkis.linares@gmail.com>
2020-05-08 09:43:16 -0500

    commit de prueba

---
+++ src/database/database.js
@@ -0,0 +1,19 @@
+import Sequelize from 'sequelize'
+
+
+export const sequelize = new Sequelize(
+    '',
+    'root',
+    '',
+    {
+        host:'localhost',
+        dialect:'mysql',
+        pool:{
+            max:5,
+            min:0,
+            require:30000,
+            idle: 10000
+        },
+        logging: false
+    }
+)
---
+++ src/models/Frases.js
@@ -0,0 +1,1 @@
+// aqui en modelo Frases
